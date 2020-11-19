9f0452f9ff87d8ef9db9cb83c92bdc55bdee476c
Yurii-Isaev <devhouse27@gmail.com>
2020-11-19 21:23:35 +0000

    feat: create drawer adapter (#1)

---
+++ app/src/main/java/com/github/mvvm/drawer/DrawerAdapter.kt
@@ -0,0 +1,61 @@
+package com.github.mvvm.drawer
+
+import android.util.SparseArray
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import androidx.recyclerview.widget.RecyclerView
+
+
+class DrawerAdapter(items: List<DrawerItem>) : RecyclerView.Adapter<DrawerAdapter.ViewHolder>() {
+    private val items: List<DrawerItem>
+    private val viewTypes: MutableMap<Class<out DrawerItem?>, Int>
+    private val holderFactories: SparseArray<DrawerItem>
+    private val listener: AdapterView.OnItemClickListener? = null
+
+    init {
+        viewTypes = HashMap<Class<out DrawerItem?>, Int>()
+        holderFactories = SparseArray<DrawerItem>()
+        processViewTypes()
+    }
+
+    private fun processViewTypes() {
+        var type = 0
+        for (item in items) {
+            if (!viewTypes.containsKey(item.getClass())) {
+                viewTypes[item.getClass()] = type
+                holderFactories.put(type, item)
+                type++
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val holder: ViewHolder = holderFactories[viewType].createViewHolder(parent)
+        holder.drawerAdapter = this
+        return holder
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        items[position].bindViewHolder(holder)
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return viewTypes[items[position].getClass()]!!
+    }
+
+
+    abstract class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
+        var drawerAdapter: DrawerAdapter? = null
+
+        override fun onClick(v: View) {}
+
+        init {
+            itemView.setOnClickListener(this)
+        }
+    }
+}
